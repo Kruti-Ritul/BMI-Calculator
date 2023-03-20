3dc6dc24647bfa624104d0d8742462ba6cc1e2bf
Kruti-Ritul <72541192+Kruti-Ritul@users.noreply.github.com>
2023-03-20 22:17:34 +0530

    20/03/2023 10:16 pm 1st page completed

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import com.example.bmicalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnnext.setOnClickListener(){
+            val Intent = Intent(this,InputData::class.java)
+            startActivity(Intent)
+        }
+
+
     }
 }
