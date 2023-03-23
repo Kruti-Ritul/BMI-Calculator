83dbb803e342f7818d5d06854b1be8eb215cd974
Kruti-Ritul <72541192+Kruti-Ritul@users.noreply.github.com>
2023-03-23 22:45:34 +0530

    23/03/2023 10:45 am Toast completed

--- app/src/main/java/com/example/bmicalculator/InputData.kt
+++ app/src/main/java/com/example/bmicalculator/InputData.kt
@@ -4,9 +4,6 @@ import android.content.Intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import com.example.bmicalculator.databinding.InputDataBinding
 import java.math.BigDecimal
@@ -24,27 +21,51 @@ class InputData : AppCompatActivity() {
 
 
 
-            if (binding.textHeight.text.isNotEmpty() && binding.textWeight.text.isNotEmpty()){
+            if (binding.textHeight.text.isNotEmpty() && binding.textWeight.text.isNotEmpty()) {
                 val weight = binding.textWeight.text.toString().toDouble()
-                val height = binding.textHeight.text.toString().toDouble()/100
+                val height = binding.textHeight.text.toString().toDouble() / 100
 
-                if(weight >= 0 && weight < 600 && height >= 0.50 && height < 2.5){
-                    val intent = Intent(this@InputData, UnderWeight::class.java)
+                if (weight > 0 && weight < 200 && height >= 0.50 && height < 2.5) {
+                    val intent = Intent(this@InputData, Result::class.java)
                     intent.putExtra("bmi", calculateBMI(weight, height))
                     startActivity(intent)
 
-                }else{
-                    if(binding.textWeight.text.isNullOrEmpty()){
-                        Toast.makeText(this,"Weight is Empty",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    if(weight <= 0 || height <= 0){
+                        Toast.makeText(this,"BMI cannot be 0",Toast.LENGTH_LONG).show()
                     }else{
-                        if(binding.textHeight.text.isNullOrEmpty()){
-                            Toast.makeText(this,"Height is Empty",Toast.LENGTH_LONG).show()
+                        if(height >= 2.5){
+                            Toast.makeText(this,"Height Out Of Range",Toast.LENGTH_LONG).show()
+                        }else{
+                            if(weight >= 200){
+                                Toast.makeText(this,"Weight Out Of Range",Toast.LENGTH_LONG).show()
+                            }else{
+                                if(height >= 2.5 && weight >= 200){
+                                    Toast.makeText(this,"Weight and Height Out Of Range",Toast.LENGTH_LONG).show()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            else{
+                if(binding.textWeight.text.isNullOrEmpty() && binding.textHeight.text.isNullOrEmpty()){
+                    Toast.makeText(this,"Fill Weight And Height ❗",Toast.LENGTH_LONG).show()
+                    }else{
+                        if(binding.textWeight.text.isNullOrEmpty()){
+                            Toast.makeText(this,"Weight is Empty",Toast.LENGTH_LONG).show()
+                        }else{
+                            if(binding.textHeight.text.isNullOrEmpty()){
+                                Toast.makeText(this,"Height is Empty",Toast.LENGTH_LONG).show()
+                            }
                         }
                     }
                 }
 
 
-            }
+
 
         }
 
--- app/src/main/java/com/example/bmicalculator/Result.kt
+++ app/src/main/java/com/example/bmicalculator/Result.kt
@@ -1,28 +1,26 @@
 package com.example.bmicalculator
 
-import android.content.Context
+import android.app.Instrumentation.ActivityResult
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintSet.Constraint
 import androidx.core.content.ContextCompat
-import com.example.bmicalculator.databinding.ActivityUnderWeightBinding
+import com.example.bmicalculator.databinding.ResultBinding
 
-class UnderWeight : AppCompatActivity() {
-    private lateinit var binding: ActivityUnderWeightBinding
+class Result : AppCompatActivity() {
+    private lateinit var binding : ResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityUnderWeightBinding.inflate(layoutInflater)
+        binding = ResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
         val bmi = intent.getDoubleExtra("bmi",-1.0)
         if (bmi<=-1.0){
-            Toast.makeText(this,"Invalid Entries ❗", Toast.LENGTH_LONG).show()
-            //binding.containerR.visibility = View.GONE
+            //Toast.makeText(this,"Invalid Entries ❗", Toast.LENGTH_LONG).show()
+            binding.containerR.visibility = View.GONE
         }
         else{
             binding.result.text = bmi.toString()
@@ -35,14 +33,14 @@ class UnderWeight : AppCompatActivity() {
             }else{
                 if(bmi > 25.9 && bmi < 30){
                     binding.containerR.setBackgroundResource(R.drawable.page4)
-                    binding.resultCard.setBackgroundResource(R.color.colorLOrange)
+                    binding.resultCard.setBackgroundResource(R.color.colorYellow)
                     binding.label.setTextColor(ContextCompat.getColor(this,R.color.colorRed))
                     binding.label.text = "You Are OverWeight ❗❗❗"
                     binding.info.text = "(normal range is 18.5 - 24.9)"
                 }else{
                     if(bmi >= 30){
                         binding.containerR.setBackgroundResource(R.drawable.page5)
-                        binding.resultCard.setBackgroundResource(R.color.colorOrange)
+                        binding.resultCard.setBackgroundResource(R.drawable.page5)
                         binding.label.setTextColor(ContextCompat.getColor(this,R.color.colorDRed))
                         binding.info.setTextColor(ContextCompat.getColor(this,R.color.colorGrey))
                         binding.label.text = "You Are Obese ❗❗❗"
